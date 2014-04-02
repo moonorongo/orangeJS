48d393f6b17df9a10b9076095b9bf1466fc7efb2
mauro <mscifu@gmail.com>
2014-04-02 19:56:34 -0300

    Agregada documentacion Sprite Finalizada documentacion Layer

--- js/class.layer.js
+++ js/class.layer.js
@@ -43,22 +43,30 @@ Orange = ( function( rootApp ){
     var _tmpCanvas;
     var _tmpCanvasBoundary;
     var orangeRoot;
-        
+
+/**
+ * @function {private void} _putInContext ejecuta _fnUpdate (que dibuja los sprites), y se encarga de pintar el layer en el canvas principal.
+ * A FUTURO: Seguramente habra algo que me permita hacer scrolles... que no se como voy a implementar, si con algo asi como una camara, o lo que sea.
+ */    
     var _putInContext = function() {
         _fnUpdate();
         _context.drawImage(_layer.canvas,0,0); // a futuro la posicion debera poder moverse. (o hacer scroll loop)
         //_context.drawImage(_boundary.canvas,0,0); // a futuro la posicion debera poder moverse. (o hacer scroll loop)
-        
     }
 
-    
+/**
+ * @function {private void} _fnUpdate Actualiza el Layer, pintando el fondo, y los Sprites asignados.
+ */    
     var _fnUpdate = function() {
-        // solo pinta la imagen de fondo si esta asignada.
         if(!_.isUndefined(_bgLayer)) { 
+            // pinto _bgLayer...
             _layer.drawImage(_bgLayer, _bgX, _bgY);
         } else {
+            // o pinta un cuadrado negro.
             _layer.fillRect(0,0,_context.canvas.width,_context.canvas.height);
         }
+        
+        // pinta cada sprite.
         _.each(_sprites, function(s) {
             s._fnUpdate();
         });
@@ -68,26 +76,42 @@ Orange = ( function( rootApp ){
     
     
     return {
-        
+/**
+ * @function {public canvas} _fnGetCanvas Devuelve el contexto del canvas
+ */    
         _fnGetCanvas : function() {
             return _layer;
         },
-        
+/**
+ * @function {public Sprites} _fnGetSprites Retorna el Array de Sprites. Realmente no se si se utiliza en algun lado...
+ */    
         _fnGetSprites : function() {
             return _sprites;
         },
-        
+
+/**
+ * @function {public void} setBackround Especifica una imagen de fondo para el Layer. Tambien se puede asignar (optativamente) una posicion inicial x e y, que es la posicion donde dibujara la imagen (por defecto: 0,0).
+ * @param {Imagen} img La imagen de fondo a especificar
+ * @param {optional} x Posicion x a dibujar.
+ * @param {optional} y Posicion y a dibujar.
+ */    
         setBackground : function(img, x,y) {
             _bgX = x || 0;
             _bgY = y || 0;
             _bgLayer = img;
         },
         
+/**
+ * @function {public void} _update Actualiza el Layer, y lo dibuja en el context principal.
+ */    
         update : function() {
             _putInContext();
         },
 
-        
+/**
+ * @function {public void} addSprite Agrega un Sprite al array interno de Sprites, del Layer.
+ * @param {Sprite} sprite El Sprite que quiero agregar.
+ */    
         addSprite : function(sprite) {
             sprite._fnSetLayer(this);
             sprite._fnSetRootContext(orangeRoot);
@@ -95,6 +119,10 @@ Orange = ( function( rootApp ){
         },
         
         
+/**
+ * @function {public boolean} removeSprite Quita la referencia del sprite del array interno de Sprites. Retorna true si la operacion se concreta exitosamente.
+ * @param {Sprite} sprite El Sprite que quiero quitar.
+ */    
         removeSprite : function(sprite) {
             var i = _sprites.indexOf(sprite);
             if(i != -1) {
@@ -103,11 +131,14 @@ Orange = ( function( rootApp ){
             } else {
                 return false;
             }
-            
-            // aca ver de retornar algo q indique si pudo hacerlo o no.
         },
 
         
+/**
+ * @function {public void} setBoundary Asigna una imagen y genera un canvas, que es utilizado para proporcionar limites de movimiento dentro del Layer (util para laberintos, plataformas y casi todo lo que se te ocurra).
+ * Por donde este en blanco la imagen se podra posicionar un Sprite, donde este en negro no sera posible.
+ * @param {Image} img La imagen a asignar.
+ */    
         setBoundary : function(img) {
             _tmpCanvasBoundary = document.createElement("canvas");
             _tmpCanvasBoundary.width = _tmpCanvas.width;
@@ -115,11 +146,20 @@ Orange = ( function( rootApp ){
             _boundary = _tmpCanvasBoundary.getContext("2d");
             _boundary.drawImage(img,0,0);
         },
+
         
+/**
+ * @function {public canvas} getBoundary Obtiene el limite asignado al Layer... No se si lo utilizo en algun lado.
+ */    
         getBoundary : function() {
             return _boundary;
         },
         
+/**
+ * @function {public Object} _fnGetBoundaryStatus Obtiene un valor de un pixel determinado, del _boundary. Devuelve un objeto {r, g, b, a}. Se utiliza para determinar si puedo posicionar un Sprite en ese lugar.
+ * @param {int} x Posicion x.
+ * @param {int} y Posicion y.
+ */    
         _fnGetBoundaryStatus : function(x,y) {
             if(_.isUndefined(_boundary)) {
                 return {
@@ -143,6 +183,10 @@ Orange = ( function( rootApp ){
         AGREGAR PROPERTY z-index, que me permita luego (mediante un setter y getter) ordenar en Orange el array _layers
         para que, al momento de dibujar, haya una estructura multicapa.
 */        
+/**
+ * @function {public void} _fnInit Utilizado por Orange.addLayer, crea una referencia _context al contexto general, e instancia el _layer interno, donde se dibujaran los Sprites.
+ * @param {canvas} context El canvas principal del juego.
+ */    
         _fnInit : function(context) {
             _context = context;
             _tmpCanvas = document.createElement("canvas");
@@ -152,7 +196,10 @@ Orange = ( function( rootApp ){
         },
         
 
-        
+/**
+ * @function {public void} _fnSetRootContext Crea una referencia interna de Orange para uso general.
+ * @param {Orange} root
+ */    
         _fnSetRootContext : function(root) {
             orangeRoot = root;
         }
--- js/class.sprite.js
+++ js/class.sprite.js
@@ -1,32 +1,80 @@
+/*
+ * Declares {@link Sprite} class etc.
+ * @file class.sprite.js
+ * @version 0.1
+  */
+
+/**
+ * @class Sprite 
+ * un sprite es un bloque que se puede mover, y que va a recibir eventos.
+ * Cada sprite puede tener asignada una ImageMap o una Animation.
+ * Si recibe un imageMap pone por defecto el primer cuadro/estado
+ * 
+ * @constructor Sprite
+ * @param {Object} customSettings Un objeto de inicializacion del Sprite. El objeto debera tener las siguientes propiedades: <br>
+ * <em>src</em> : ImageMap o Animation utilizada por el Sprite <br>
+ * optional int <em>pivotX</em>: el punto utilizado como limite contra el _boundary Layer.<br>
+ * optional int <em>pivotY</em>: idem <br>
+ * optional int <em>class</em>: Un identificador que indica a que clase pertenece. Puedes utilizar algunos de los identificadores predefinidos internamente, o  crear indicadores propios<br>
+ * optional <em>speed</em>: la velocidad con la que avanzara... no demasiado util, todavia<br>
+ */
 Orange = ( function( rootApp ){
 
-    
-  // customSettings.src puede ser ImageMap o Animation
   rootApp.Sprite = function(customSettings){
+/** @property {private Layer} _layer Referencia al Layer que pertenece. */
     var _layer,
+/** @property {private int} _x Posicion x del Sprite. */
         _x = 0,
+/** @property {private int} _y Posicion y del Sprite. */
         _y = 0,
         _pivot,
+        
+/** @property {private int} _speed Velocidad interna. */
         _speed = customSettings.speed || 1,
+        
+/** @property {private Object} _eventCallback Objecto interno con los distintos callbacks de los eventos asignados al Sprite. */
         _eventCallback = {},
+        
+/** @property {private ImageMap} _src Imagen asociada al Sprite, puede ser un ImageMap o una Animation. */
         _src = customSettings.src,
+        
+/** @property {private int} _w Ancho del Sprite. */
         _w = _src.getSpriteWidth(),
+/** @property {private int} _h Alto del Sprite. */
         _h = _src.getSpriteHeight(),
+           
+/** @property {private int} _pivotX Punto utilizado para ver si se puede colocar el sprite en esa posicion. */
         _pivotX = customSettings.pivotX || Math.floor(_w / 2),
+/** @property {private int} _pivotY Punto utilizado para ver si se puede colocar el sprite en esa posicion.  */
         _pivotY = customSettings.pivotY || Math.floor(_h / 2),
+           
+/** @property {private int} _class Numero interno para clasificar de alguna manera al Sprite. */
         _class = customSettings.class || 0,
+/** @property {private int} _dirX Direccion horizontal hacia donde esta yendo el Sprite. */
         _dirX = 0,
+/** @property {private int} _dirY Direccion vertical hacia donde esta yendo el Sprite. */
         _dirY = 0,
-        name, 
+//        name, 
         e, 
-        extra,
+//        extra,
+        
+/** @property {private boolean} _prepareToDestroy Flag, se pone en true cuando llamo a Sprite.destroy(). */
         _prepareToDestroy = false,
+        
+/** @property {private boolean} _removeFromLayer Flag, cuando llamo a Sprite.destroy() indica si debo, ademas, quitar del Layer el sprite. 
+ * Esto es util para cuando quiero destruir un Sprite, pero quiero dejar el "cadaver" como imagen. 
+ */
         _removeFromLayer = false,
+        
+/** @property {private int} _muriendo Numero que tiene la cantidad de frames que dura la animacion que se ejecuta cuando destruyo al Sprite. */
         _muriendo = (_src.getType() == "Animation")?  _src._fnGetStatusDieCantFrames() : 1;
     
     var orangeRoot;         
     
-    // setea la direccion de avance de acuerdo a la posicion suministrada
+/**
+ * @function {private void} _setDirX Establece la direccion de movimiento del Sprite.
+ * @param {int} x nueva posicion.
+ */    
     var _setDirX = function(x) {
         if(x > _x) {
             _dirX = Orange.Sprite.MOVE_RIGHT;
@@ -38,8 +86,10 @@ Orange = ( function( rootApp ){
     }
         
         
-        
-    // setea la direccion de avance de acuerdo a la posicion suministrada
+/**
+ * @function {private void} _setDirY Establece la direccion de movimiento del Sprite.
+ * @param {int} y nueva posicion.
+ */    
     var _setDirY = function(y) {
         if(y > _y) {
             _dirY = Orange.Sprite.MOVE_UP;
@@ -51,6 +101,10 @@ Orange = ( function( rootApp ){
     }   
     
     
+/**
+ * @function {private void} _setX Posiciona el Sprite en la pantalla. Checkea si puede posicionarlo, y actualiza la direccion de movimiento.
+ * @param {int} x nueva posicion.
+ */    
     var _setX = function(x) {
         _setDirX(x);
 
@@ -69,7 +123,10 @@ Orange = ( function( rootApp ){
         } // puedo posicionar
     }; // setX
 
-    
+/**
+ * @function {private void} _setY Posiciona el Sprite en la pantalla. Checkea si puede posicionarlo, y actualiza la direccion de movimiento.
+ * @param {int} y nueva posicion.
+ */    
     var _setY = function(y) {
         _setDirY(y);
         
